b198114e60344c7584612e2070945b9598d63070
Amrit Raj <amrit.21ug2053@iiitranchi.ac.in>
2024-11-16 01:44:11 +0530

    added frontend url

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,7 @@ app.use(bodyParser.json());
 
 // CORS configuration
 const allowedOrigins = [
-  "http://localhost:3000", // Local development
-  "https://car-management-application-57xc.vercel.app/", // Replace with your deployed frontend URL
+  "https://car-management-application-phi.vercel.app/", // Replace with your deployed frontend URL
 ];
 app.use(
   cors({
